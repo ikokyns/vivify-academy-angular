be2c8bf036f398550b78a033cc7759915493c2fe
Danilo Banjanin <danilo216@gmail.com>
2017-08-21 14:25:22 +0200

    Commit 3 - Include template file

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contacts',
-  template: '<h1>Contacts</h1>'
+  templateUrl: 'contacts.component.html'
 })
 export class ContactsComponent {
 
