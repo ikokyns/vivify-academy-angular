e35c19acf3b5b579155449c8fabb15c4f9eaf262
Danilo Banjanin <danilo216@gmail.com>
2017-08-21 14:21:49 +0200

    Commit 2 - Created contacts component

---
+++ src/app/contacts/contacts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts',
+  template: '<h1>Contacts</h1>'
+})
+export class ContactsComponent {
+
+  constructor() { }
+
+}
